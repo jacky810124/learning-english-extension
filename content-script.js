57da0f3dd0f4b792fa17ef49a0701541868953d1
jacky810124 <kang810124@gmail.com>
2023-01-02 23:19:40 +0800

    🔨 Refactor: add worker

--- content-script.js
+++ content-script.js
@@ -1,34 +1,7 @@
-const setDebug = ({ namespace = "" } = {}) => {
-  const namespaceString = namespace
-    .split(/[\s\n,]/)
-    .filter((i) => i)
-    .join(",");
-  localStorage.debug = namespaceString;
-  const sendMessageToSW = () => {
-    navigator.serviceWorker.controller.postMessage({
-      type: "ENABLE_DEBUG",
-      payload: { namespace: namespaceString },
-    });
-  };
-  if (!navigator.serviceWorker.controller) {
-    navigator.serviceWorker.addEventListener("controllerchange", (event) => {
-      sendMessageToSW();
-    });
-  } else {
-    sendMessageToSW();
-  }
-};
-const syncNamespace = async () => {
-  const { namespace = "" } = await chrome.storage.sync.get(["namespace"]);
-  console.log("namespace@content-script", namespace);
-  setDebug({ namespace });
-};
-const main = async () => {
-  console.log("main@content-scripts");
-  chrome.storage.onChanged.addListener(async (changes) => {
-    syncNamespace();
-  });
-  syncNamespace();
-};
-
-main();
+// web worker
+// fetch(chrome.runtime.getURL("/worker.js"))
+//   .then((res) => res.blob())
+//   .then((blob) => URL.createObjectURL(blob))
+//   .then((objectUrl) => {
+//     const worker = new Worker(objectUrl);
+//   });
